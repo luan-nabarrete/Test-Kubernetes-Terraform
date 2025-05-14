d245630bbc0ccf700824c312862498653500aa01
Luan Nabarrete <luan.nabarrete@darede.com.br>
2025-05-14 04:37:23 -0300

    update cluster.yaml

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "User data executado" >> /tmp/userdata.log
 
-
+# Install Git and Docker
 yum update -y
-yum install -y git
+yum install -y git docker
+
+systemctl start docker.service
+systemctl enable docker.service
 
 # Install kind, helm and kubectl
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
@@ -12,24 +15,23 @@ mv ./kind /usr/local/bin/kind
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/v1.30.1/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin/
 
-
+# Cloning helm dir from repo
 mkdir /home/ec2-user/nginx-web-server/
-
 cd /home/ec2-user/nginx-web-server/
-
 git init
 git remote add origin https://github.com/luan-nabarrete/Test-Kubernetes-Terraform.git
-
 git config core.sparseCheckout true
 echo "helm/" >> .git/info/sparse-checkout
-
 git pull origin main
 
-kind create cluster --name nginx-web-server-cluster --config ./kind-app-nginx-cluster.yaml
+# Create kind cluster
+kind create cluster --name nginx-web-server-cluster --config ./helm/kind-app-nginx-cluster.yaml
 
+kubectl config use-context kind-nginx-web-server-cluster
 
-
+# Install Helm Chart
+helm install nginx-web-server ./helm/
